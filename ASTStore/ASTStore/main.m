c0f83b19678c05af031f47425e8faeb3714a84c7
Sean Kormilo <sean.kormilo@anystonetech.com>
2011-03-07 15:02:33 -0500

    Setup some more initial projects

---
+++ ASTStore/ASTStore/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ASTStore
+//
+//  Created by Sean Kormilo on 11-03-07.
+//  Copyright 2011 Anystone Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
