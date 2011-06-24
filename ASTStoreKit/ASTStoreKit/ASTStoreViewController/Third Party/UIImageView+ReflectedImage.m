bcf3a45a0f2c6c6c83a6b88259989a43a035157c
Sean Kormilo <sean.kormilo@anystonetech.com>
2011-06-24 15:57:03 -0400

    [bugzid:917]

---
+++ ASTStoreKit/ASTStoreKit/ASTStoreViewController/Third Party/UIImageView+ReflectedImage.m
@@ -0,0 +1,172 @@
+//
+//  UIImageView+ReflectedImage.m
+//  ASTStoreKit
+//
+//  Created by Sean Kormilo on 11-06-24.
+//  http://www.anystonetech.com
+
+//  Copyright (c) 2011 Anystone Technologies, Inc.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+
+// The bones of this functionality came from the Apple Reflection Sample Code
+// http://developer.apple.com/library/ios/#samplecode/Reflection/Introduction/Intro.html
+//
+// And carries the following license:
+
+/*
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+ Inc. ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or redistribution of
+ this Apple software constitutes acceptance of these terms.  If you do
+ not agree with these terms, please do not use, install, modify or
+ redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following terms, and
+ subject to these terms, Apple grants you a personal, non-exclusive
+ license, under Apple's copyrights in this original Apple software (the
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple
+ Software, with or without modifications, in source and/or binary forms;
+ provided that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the following
+ text and disclaimers in all such redistributions of the Apple Software.
+ Neither the name, trademarks, service marks or logos of Apple Inc. may
+ be used to endorse or promote products derived from the Apple Software
+ without specific prior written permission from Apple.  Except as
+ expressly stated in this notice, no other rights or licenses, express or
+ implied, are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or by other
+ works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ Copyright (C) 2010 Apple Inc. All Rights Reserved.
+*/
+
+
+#import "UIImageView+ReflectedImage.h"
+
+
+@implementation UIImageView (ReflectedImage)
+
+CGImageRef CreateGradientImage(int pixelsWide, int pixelsHigh)
+{
+	CGImageRef theCGImage = NULL;
+    
+	// gradient is always black-white and the mask must be in the gray colorspace
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+	
+	// create the bitmap context
+	CGContextRef gradientBitmapContext = CGBitmapContextCreate(NULL, pixelsWide, pixelsHigh,
+															   8, 0, colorSpace, kCGImageAlphaNone);
+	
+	// define the start and end grayscale values (with the alpha, even though
+	// our bitmap context doesn't support alpha the gradient requires it)
+	CGFloat colors[] = {0.0, 1.0, 1.0, 1.0};
+	
+	// create the CGGradient and then release the gray color space
+	CGGradientRef grayScaleGradient = CGGradientCreateWithColorComponents(colorSpace, colors, NULL, 2);
+	CGColorSpaceRelease(colorSpace);
+	
+	// create the start and end points for the gradient vector (straight down)
+	CGPoint gradientStartPoint = CGPointZero;
+	CGPoint gradientEndPoint = CGPointMake(0, pixelsHigh);
+	
+	// draw the gradient into the gray bitmap context
+	CGContextDrawLinearGradient(gradientBitmapContext, grayScaleGradient, gradientStartPoint,
+								gradientEndPoint, kCGGradientDrawsAfterEndLocation);
+	CGGradientRelease(grayScaleGradient);
+	
+	// convert the context into a CGImageRef and release the context
+	theCGImage = CGBitmapContextCreateImage(gradientBitmapContext);
+	CGContextRelease(gradientBitmapContext);
+	
+	// return the imageref containing the gradient
+    return theCGImage;
+}
+
+CGContextRef MyCreateBitmapContext(int pixelsWide, int pixelsHigh)
+{
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	
+	// create the bitmap context
+	CGContextRef bitmapContext = CGBitmapContextCreate (NULL, pixelsWide, pixelsHigh, 8,
+														0, colorSpace,
+														// this will give us an optimal BGRA format for the device:
+														(kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst));
+	CGColorSpaceRelease(colorSpace);
+    
+    return bitmapContext;
+}
+
+- (UIImage *)reflectedImageWithHeight:(NSUInteger)height
+{
+    if(height == 0)
+		return nil;
+    
+	// create a bitmap graphics context the size of the image
+	CGContextRef mainViewContentContext = MyCreateBitmapContext(self.bounds.size.width, height);
+	
+	// create a 2 bit CGImage containing a gradient that will be used for masking the 
+	// main view content to create the 'fade' of the reflection.  The CGImageCreateWithMask
+	// function will stretch the bitmap image as required, so we can create a 1 pixel wide gradient
+	CGImageRef gradientMaskImage = CreateGradientImage(1, height);
+	
+	// create an image by masking the bitmap of the mainView content with the gradient view
+	// then release the  pre-masked content bitmap and the gradient bitmap
+	CGContextClipToMask(mainViewContentContext, CGRectMake(0.0, 0.0, self.bounds.size.width, height), gradientMaskImage);
+	CGImageRelease(gradientMaskImage);
+	
+	// In order to grab the part of the image that we want to render, we move the context origin to the
+	// height of the image that we want to capture, then we flip the context so that the image draws upside down.
+	CGContextTranslateCTM(mainViewContentContext, 0.0, height);
+	CGContextScaleCTM(mainViewContentContext, 1.0, -1.0);
+	
+	// draw the image into the bitmap context
+	CGContextDrawImage(mainViewContentContext, self.bounds, self.image.CGImage);
+	
+	// create CGImageRef of the main view bitmap content, and then release that bitmap context
+	CGImageRef reflectionImage = CGBitmapContextCreateImage(mainViewContentContext);
+	CGContextRelease(mainViewContentContext);
+	
+	// convert the finished reflection image to a UIImage 
+	UIImage *theImage = [UIImage imageWithCGImage:reflectionImage];
+	
+	// image is retained by the property setting above, so we can release the original
+	CGImageRelease(reflectionImage);
+	
+	return theImage;
+}
+
+
+@end
